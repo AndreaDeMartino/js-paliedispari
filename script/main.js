fb718cff9719836a5856b9a37f26eb84eacd8dd1
Andrea De Martino <ademartino1992@gmail.com>
2020-04-02 16:14:17 +0200

    Creation: Js Logic

--- script/main.js
+++ script/main.js
@@ -1,10 +1,82 @@
-// Palidroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+// Palidromo
+alert('Benvenuto al gioco del Palindromo');
+
+var userWord = prompt('Inserisci una parola, non preoccuparti del case sensitive').toLowerCase();
+
+if ( userWord == (palindromo(userWord)) ){
+  alert('La parola digitata è palindroma');
+} else{
+  alert('La parola digitata NON è palindroma')
+}
+
+function palindromo (word){
+  var reverseWord = '';
+  
+  for (var i = (word.length - 1); i >= 0 ; i--){
+    reverseWord += word[i];
+  }
+
+  return reverseWord;
+}
+
 // Pari e Dispari
-// L’utente sceglie pari o dispari e un numero da 1 a 5. 
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-// Ricordate di dare un occhio alle slide, che sono smepre utili
+alert('Benvenuto al gioco Pari o Dispari');
+
+var userChoice = prompt('Scegli pari o dispari').toUpperCase();
+console.log('Scelta utente: ' + userChoice);
+
+// Respingo l'utente se la scelta è un numero o se è diversa da pari / dispari, con fix su Case Sensitive
+if  ( (userChoice == isNaN) || 
+    ((userChoice !== 'PARI' && userChoice !== 'DISPARI'))
+    ){
+  alert('Scelta errata');
+} 
+
+else{
+
+var userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
+console.log('Numero Utente: ' + userNumber);
+
+// Verifico che la scelta sia coerente con il range richiesto
+if (userNumber == 0 || userNumber < 1 || userNumber > 5){
+  alert('Scelta errata');
+}
+
+else{
+
+var computerNumber = randomNumber();
+console.log('Numero Computer: ' + computerNumber);
+alert('Il computer ha scelto il numero: ' + computerNumber);
+
+function randomNumber (){
+  var number = Math.floor(Math.random() * 5 + 1);
+  return number;
+}
+
+var somma = userNumber + computerNumber;
+console.log('Somma dei due numeri = ' + somma);
+alert('La somma dei due numeri è: ' + somma);
+
+if (pariDispari(somma) == 'PARI'){
+  alert('La somma dei due numeri è pari');
+} else{
+  alert('La somma dei due numeri è dispari');
+}
+
+if (pariDispari(somma) == userChoice) {
+  alert('Il giocatore vince');
+} else{
+  alert('Il computer vince');
+}
+
+function pariDispari (numberCheck){
+  var check = '';
+  if ( (numberCheck % 2) == 0 ){
+    check = 'PARI';
+  } else{
+    check = 'DISPARI';
+  }
+  return check;
+}
+}
+}
